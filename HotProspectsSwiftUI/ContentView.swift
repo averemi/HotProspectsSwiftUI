5b8c20862652aaad631ace4c49f615609623f19c
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-10-14 23:04:46 +0200

    Added async await and result.

--- HotProspectsSwiftUI/ContentView.swift
+++ HotProspectsSwiftUI/ContentView.swift
@@ -24,10 +24,31 @@ import SwiftUI
 }
 
 struct ContentView: View {
-    @StateObject private var updater = DisplayedUpdated()
+    @State private var output = ""
     
     var body: some View {
-        Text("Value is \(updater.value)")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")
+            let (data, _) = try await URLSession.shared.data(from: url!)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
     }
 }
 
