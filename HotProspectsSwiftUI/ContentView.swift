2553f14a9511543ff723f561c87398b0f85885d6
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-10-14 22:14:25 +0200

    Added Environment object.

--- HotProspectsSwiftUI/ContentView.swift
+++ HotProspectsSwiftUI/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    @StateObject var user = User()
+    
+    var body: some View {
+        VStack {
+            EditView().environmentObject(user)
+            DisplayView().environmentObject(user)
+        }
     }
 }
 
