89329e92c5423bb848326f77174df6f9c9762872
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-10-14 21:56:29 +0200

    Project setup.

---
+++ HotProspectsSwiftUI/HotProspectsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsSwiftUIApp.swift
+//  HotProspectsSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
