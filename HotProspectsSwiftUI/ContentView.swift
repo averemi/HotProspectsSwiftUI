b5f2a142019c05edc329f395046b1bab592a2789
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2023-07-23 21:52:14 +0200

    Added FilterType enum to destinguish between contact types.

--- HotProspectsSwiftUI/ContentView.swift
+++ HotProspectsSwiftUI/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            ProspectsView()
+            ProspectsView(filter: .none)
                 .tabItem {
                     Label("Everyone", systemImage: "person.3")
                 }
             
-            ProspectsView()
+            ProspectsView(filter: .contacted)
                 .tabItem {
                     Label("Contacted", systemImage: "checkmark.circle")
                 }
             
-            ProspectsView()
+            ProspectsView(filter: .uncontacted)
                 .tabItem {
                     Label("Uncontacted", systemImage: "questionmark.diamond")
                 }
--- HotProspectsSwiftUI/ProspectsView.swift
+++ HotProspectsSwiftUI/ProspectsView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+
+    let filter: FilterType
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle(title)
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted People"
+        case .uncontacted:
+            return "Uncontacted People"
+        }
     }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProspectsView()
+        ProspectsView(filter: .none)
     }
 }
