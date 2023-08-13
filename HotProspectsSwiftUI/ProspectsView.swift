db1bfa6ea60277f2fba04680bd679227b6545304
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2023-08-13 18:16:41 +0200

    Added QR code scanning.

--- HotProspectsSwiftUI/ProspectsView.swift
+++ HotProspectsSwiftUI/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Anastasiia Veremiichyk on 23/07/2023.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -13,6 +14,8 @@ struct ProspectsView: View {
     }
 
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
+
     let filter: FilterType
 
     var body: some View {
@@ -30,13 +33,13 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button {
-                    var prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@gmail.com"
-                    prospects.people.append(prospect)
+                    isShowingScanner = true
                 } label: {
                     Label("Scan", systemImage: "qrcode.viewfinder")
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaulhudson@gmail.com", completion: handleScan)
             }
         }
     }
@@ -60,6 +63,24 @@ struct ProspectsView: View {
             return prospects.people.filter { $0.isContacted }
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted }
+        }
+    }
+
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            var person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
